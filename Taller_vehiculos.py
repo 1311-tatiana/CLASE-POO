d33b5151291a78e6cadc354765be3213148d9e81
Your Name <you@example.com>
2025-10-29 11:26:34 -0500

    taller

---
+++ Taller_vehiculos.py
@@ -0,0 +1,26 @@
+class Empresa:
+    def __init__(self, nombre, vehiculo):
+        self.nombre = nombre 
+        self.vehiculo = vehiculo 
+    
+    def mostrar_informacion(self):
+        pass 
+
+    def resumen_flota(self):
+        pass
+
+    def guargar_en_archivo(self):
+        pass
+
+    
+class Vehiculo(Empresa):
+    def __init__(self, placa, modelo, marca):
+        self.placa = placa 
+        self.modelo = modelo 
+        self.marca = marca 
+
+class Moto(Vehiculo):
+
+class Camion(Vehiculo):
+
+class Autoparticular(Vehiculo):
